ac60e639bf8e8532b7f1c23c24795307e4e3512d
Ivan <vipanda569@mail.ru>
2025-03-12 16:58:42 +0700

    Fixed i18next

--- src/shared/i18next/consts/index.ts
+++ src/shared/i18next/consts/index.ts
@@ -1,5 +1,5 @@
 const loadPathBeforeProd=`./locales/{{lng}}/{{ns}}.json`;
-const loadPathAfterProd=`/*/locales/{{lng}}/{{ns}}.json`;
+const loadPathAfterProd=`/gems-app/locales/{{lng}}/{{ns}}.json`;
 
 export const loadPathBeforeAndAfterProd =
   process.env.NODE_ENV === "production"
