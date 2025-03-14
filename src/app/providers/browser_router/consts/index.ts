57f99314d81037f3062e980966ccd59e3bddeb12
Ivan <vipanda569@mail.ru>
2025-03-14 15:12:57 +0700

    Added 404.html, fixed index.html

--- src/app/providers/browser_router/consts/index.ts
+++ src/app/providers/browser_router/consts/index.ts
@@ -1,3 +1,3 @@
-export const url ="/gems-app" //import.meta.env.VITE_REACT_API_URL;  
+export const url = import.meta.env.VITE_REACT_API_URL;
 
-export const baseUrl = "/" // import.meta.env.VITE_REACT_API_BASE_URL; 
+export const baseUrl = import.meta.env.VITE_REACT_API_BASE_URL;
--- src/app/providers/browser_router/ui/CustomBrowserRouter.tsx
+++ src/app/providers/browser_router/ui/CustomBrowserRouter.tsx
@@ -1,6 +1,6 @@
 import { memo, ReactNode } from "react";
 import { baseName } from "@/app/providers/browser_router/config/baseName";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
 interface CustomBrowserRouterProps {
   children: ReactNode;
