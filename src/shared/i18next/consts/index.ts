b061ad77b5dab0c204110db6db4be819d2d55646
Ivan <vipanda569@mail.ru>
2025-03-14 18:04:31 +0700

    Changed in file index.ts

--- src/shared/i18next/consts/index.ts
+++ src/shared/i18next/consts/index.ts
@@ -2,9 +2,7 @@ const loadPathBeforeProd = `/locales/{{lng}}/{{ns}}.json`;
 
 const url = "gems-app";
 
-const checkUrl = url === "gems-app" ? url : "docs";
-
-const loadPathAfterProd = `/${checkUrl}/locales/{{lng}}/{{ns}}.json`;
+const loadPathAfterProd = `/${url}/locales/{{lng}}/{{ns}}.json`;
 
 export const loadPathBeforeAndAfterProd =
   process.env.NODE_ENV === "production"
