951b7d04edf923e96f75b1dba7087f3e07632df9
Ivan <vipanda569@mail.ru>
2025-03-14 17:20:56 +0700

    Checked url

--- src/shared/i18next/consts/index.ts
+++ src/shared/i18next/consts/index.ts
@@ -1,7 +1,12 @@
-const loadPathBeforeProd=`/locales/{{lng}}/{{ns}}.json`;
-const loadPathAfterProd=`/gems-app/locales/{{lng}}/{{ns}}.json`;
+const loadPathBeforeProd = `/locales/{{lng}}/{{ns}}.json`;
+
+const url = "gems-app";
+
+const checkUrl = url === "gems-app" ? url : "gems-app-storybook.vercel.app";
+
+const loadPathAfterProd = `/${checkUrl}/locales/{{lng}}/{{ns}}.json`;
 
 export const loadPathBeforeAndAfterProd =
   process.env.NODE_ENV === "production"
-    ? loadPathAfterProd 
+    ? loadPathAfterProd
     : loadPathBeforeProd;
